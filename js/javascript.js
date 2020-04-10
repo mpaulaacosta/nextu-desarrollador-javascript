77a53424f40b32ebf8bc77bd996337b9cf8a6d74
Carlos J. Acosta <ing1931-azu@yahoo.com>
2020-04-10 12:15:21 -0500

    JS separado

---
+++ js/javascript.js
@@ -0,0 +1,26 @@
+function inicializar() {
+  // 5.2 tipos de literales
+  document.getElementById("n1").innerHTML = 'numéricos como: 2020 23.4 45e3';
+  document.getElementById('a1').innerHTML =
+    "strings: texto puede ser con comillas dobles o sencillas";
+  // 6.1 Objetos y funciones @ 5:39
+  qClicks=0;
+}
+
+function clickEnBoton() {
+  qClicks++;
+  document.getElementById("bClicks").innerHTML = qClicks;
+}
+
+function generarError() {
+  //numero1=0;
+  try {
+    numero = numero1 * 1;
+    throw('error personalizado');
+    alert("no se ejecutan");
+  } catch (e) {
+    alert(e);
+  } finally {
+    // no entiendo para qué
+  }
+}
